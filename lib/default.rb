82568ff4922d4fa912624422df47e77aa4758fab
Yaw Odame <yoodame@uchicago.edu>
2014-06-23 14:02:24 -0500

    toolkit dev

--- lib/default.rb
+++ lib/default.rb
@@ -2,8 +2,13 @@
 # before nanoc starts compiling.
 
 module SideNavHelper extend self
-  def api_menu(items)
+  def menu_api(items)
   	i = items.select{|x| x.identifier.start_with?("/docs/transfer-api")}
+		i.sort_by { |a| a[:menu_weight] }
+	end
+
+	def menu_toolkit(items)
+		i = items.select{|x| x.identifier.start_with?("/docs/toolkit")}
 		i.sort_by { |a| a[:menu_weight] }
 	end
 end
