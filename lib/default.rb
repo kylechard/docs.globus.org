9963bc2d47ca7cf49a8e905854db628c32c28318
Yaw Odame <yoodame@uchicago.edu>
2014-06-24 11:52:03 -0500

    toolkit dev with side nav

--- lib/default.rb
+++ lib/default.rb
@@ -4,12 +4,16 @@
 module SideNavHelper extend self
   def menu_api(items)
   	i = items.select{|x| x.identifier.start_with?("/docs/transfer-api")}
-		i.sort_by { |a| a[:menu_weight] }
+  	self.sort_menuitems(i,':menu_weight')
+	end
+
+	def sort_menuitems(items, sort_by)
+		items.sort_by { |a| a[sort_by] }
 	end
 
 	def menu_toolkit(items)
 		i = items.select{|x| x.identifier.start_with?("/docs/toolkit")}
-		i.sort_by { |a| a[:menu_weight] }
+		self.sort_menuitems(i,':menu_weight')
 	end
 end
 
