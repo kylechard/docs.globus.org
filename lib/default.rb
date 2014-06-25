aa55af29f3c4dadb996ff3f002668485a2add95c
Yaw Odame <yoodame@uchicago.edu>
2014-06-25 09:59:28 -0500

    breadcrumbs

--- lib/default.rb
+++ lib/default.rb
@@ -3,7 +3,7 @@
 
 module SideNavHelper extend self
   def menu_api(items)
-  	i = items.select{|x| x.identifier.start_with?("/docs/transfer-api")}
+  	i = items.select{|x| x.identifier.start_with?("/transfer-api/docs")}
   	self.sort_menuitems(i,':menu_weight')
 	end
 
@@ -12,7 +12,7 @@ module SideNavHelper extend self
 	end
 
 	def menu_toolkit(items)
-		i = items.select{|x| x.identifier.start_with?("/docs/toolkit")}
+		i = items.select{|x| x.identifier.start_with?("/toolkit/docs")}
 		self.sort_menuitems(i,':menu_weight')
 	end
 end
