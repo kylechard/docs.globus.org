3f6cb47860922e8c552675679791c173e871c35e
Yaw Odame <yoodame@uchicago.edu>
2014-09-25 10:15:54 -0500

    toolkit filters

--- lib/filters.rb
+++ lib/filters.rb
@@ -33,36 +33,17 @@ module Nanoc::Filters
       doc = Nokogiri::HTML(content)
       
       doc.css('head')[0].remove
-      # doc.css('div.titlepage')[0].remove
-      # doc.css('div.titlepage').css('hr').remove
 
-      # doc.css('table').add_class('table').remove_attr('width')
+      doc.css('div.titlepage')[0].remove
+      doc.css('div.titlepage').css('hr').remove
 
-      # doc.css("table[border='1']").add_class('table-bordered')
+      doc.css('table').add_class('table').remove_attr('width')
+      doc.css("table[border='1']").add_class('table-bordered')
       
-      # doc.css("div.note").remove_attr('style').add_class('alert alert-info')
-      # doc.css("div.important").remove_attr('style').add_class('alert alert-warning')
-      # doc.css("div.warning").remove_attr('style').add_class('alert alert-danger')
-      # doc.css("div.tip").remove_attr('style').add_class('alert alert-info')
-      
-      # puts '-----------------------------------'
-      # puts links.inspect
-
-      # doc.search('//div.titlepage').each do |node|
-      #   # node.remove
-      #   puts node
-      #   # puts node
-      #   # node.children.remove
-      #   # node.content = 'Children removed.'
-      # end
-
-      # Find comments.
-      # doc.xpath("//comment()").each do |comment|
-      #     # Check it's not a conditional comment.
-      #     if (comment.content !~ /\A(\[if|\<\!\[endif)/)
-      #         comment.remove()
-      #     end
-      # end
+      doc.css("div.note").remove_attr('style').add_class('alert alert-info')
+      doc.css("div.important").remove_attr('style').add_class('alert alert-warning')
+      doc.css("div.warning").remove_attr('style').add_class('alert alert-danger')
+      doc.css("div.tip").remove_attr('style').add_class('alert alert-info')
 
       doc.to_html
     end
