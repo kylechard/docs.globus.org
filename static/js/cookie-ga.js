1ccd4ddf39326682b98109fd66bc1c24b56bd78c
teresas <teresas@ci.uchicago.edu>
2015-09-02 17:37:19 -0500

    added cookie notice

---
+++ static/js/cookie-ga.js
@@ -0,0 +1,38 @@
+$(function() {
+    // Turn off GA for now
+    window['ga-disable-UA-19656330-29'] = true;
+
+    // Check if user has already said ok to cookies
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return "";
+    }
+
+    var cookie_status=getCookie("gdev_cookies");
+    if (cookie_status!="") {
+        window['ga-disable-UA-19656330-29'] = false; // Turn on GA
+    }else{
+        // Check if user from EU country
+        var eu_countries = ["AL", "AD", "AM", "AT", "AZ", "BY", "BE", "BA", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "GE", 
+            "DE", "GR", "HU", "IS", "IE", "IT", "KZ", "XK", "LV", "LI", "LT", "LU", "MK", "MT", "MD", "MC", "ME", "NL", "NO", "PL", 
+            "PT", "RO", "RU", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "TR", "UA", "GB", "VA"];
+
+        $.get("http://ipinfo.io", function(response) {
+            if($.inArray(response.country, eu_countries)) {
+                $('#cookie_notice').modal('show'); // show cookie notice if EU country
+                $('#cookie_notice').on('hidden.bs.modal', function () {
+                    document.cookie="gdev_cookies=true"; // set cookie status to true
+                })
+            }else{
+                document.cookie="gdev_cookies=true"; // set cookie status to true
+            }
+        }, "jsonp");
+    }
+
+});
