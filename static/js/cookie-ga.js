b89aee09ade9cf26691e9ca8e921437d262d1ac0
teresas <teresas@ci.uchicago.edu>
2015-09-03 13:33:57 -0500

    adjusted cookie js

--- static/js/cookie-ga.js
+++ static/js/cookie-ga.js
@@ -1,6 +1,5 @@
 $(function() {
-    // Turn off GA for now
-    window['ga-disable-UA-19656330-29'] = true;
+    window['ga-disable-UA-19656330-29'] = true; // Turn off GA for now
 
     // Check if user has already said ok to cookies
     function getCookie(cname) {
@@ -21,18 +20,24 @@ $(function() {
         // Check if user from EU country
         var eu_countries = ["AL", "AD", "AM", "AT", "AZ", "BY", "BE", "BA", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "GE", 
             "DE", "GR", "HU", "IS", "IE", "IT", "KZ", "XK", "LV", "LI", "LT", "LU", "MK", "MT", "MD", "MC", "ME", "NL", "NO", "PL", 
-            "PT", "RO", "RU", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "TR", "UA", "GB", "VA"];
+            "PT", "RO", "RU", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "TR", "UA", "GB", "VA", "US"];
 
-        $.get("http://ipinfo.io", function(response) {
-            if($.inArray(response.country, eu_countries)) {
-                $('#cookie_notice').modal('show'); // show cookie notice if EU country
-                $('#cookie_notice').on('hidden.bs.modal', function () {
+        $.ajax( { 
+            url: '//freegeoip.net/json/', // limited to 10,000 queries per hour
+            type: 'POST', 
+            dataType: 'jsonp',
+            success: function(location) {
+                console.log(location.country_code);
+                if($.inArray(location.country_code, eu_countries) > -1) {
+                    $('#cookie_notice').modal('show'); // show cookie notice if EU country
+                    $('#cookie_notice').on('hidden.bs.modal', function () {
+                        document.cookie="gdev_cookies=true"; // set cookie status to true
+                    })
+                }else{
                     document.cookie="gdev_cookies=true"; // set cookie status to true
-                })
-            }else{
-                document.cookie="gdev_cookies=true"; // set cookie status to true
+                }
             }
-        }, "jsonp");
+        });
     }
 
 });
