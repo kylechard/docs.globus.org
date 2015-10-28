5cd1c3ef868f76b89164351c701ce0ec1cc23f13
Stephen Rosen <sirosen@uchicago.edu>
2015-10-28 11:04:15 -0500

    Move docs sites to S3

--- deploy.sh
+++ deploy.sh
@@ -20,34 +20,12 @@ then
     exit 1
 fi
 
-
-### Config ###
-# Current branch
-branch="$(git rev-parse --abbrev-ref HEAD)"
-
-
-### Setup for staging deployment
+## check env to set destination bucket
 if [ "$env" == "staging" ]
 then
-    domain_name="docs.staging.globuscs.info"
-    repo_dir='docs.globus.org-staging'
-    repo_url='git@github.com:globusonline/docs.globus.org-staging.git'
-fi
-
-
-### Setup for prod deployment
-if [ "$env" == "production" ]
-then
-    # ensure that current branch is prod
-    if [ "$branch" != "prod" ]
-    then
-        echo "Sorry! You must be on 'prod' branch to deploy to production."
-        exit 1
-    fi
-
-    domain_name="github-pages.docs.globus.org"
-    repo_dir='docs.globus.org'
-    repo_url='git@github.com:globusonline/docs.globus.org.git'
+    bucket="docs.staging.globuscs.info"
+else
+    bucket="docs.globus.org"
 fi
 
 
@@ -58,45 +36,13 @@ echo "Deploying to $env"
 ./install_asciidoc_backend.sh
 
 
-# Remove repo if exists
-if [ -d "$repo_dir" ]; then
-  rm -rf "$repo_dir"
-fi
-# Clone repo and clean
-git clone "$repo_url" "$repo_dir"
-cd "$repo_dir"
-git checkout gh-pages
-git add -u
-git rm -r ./*
-rm -rf ./*
-
-
-
 
 # Build latest
-echo "Compiling nanoc"
-cd ../
+echo "Compiling site with nanoc"
 rm -rf output
 nanoc
 
-
-# Build the correct CNAME file for the env
-# Commit and push the changes to the sites dir
-cd "$repo_dir"
-
-echo "Generating CNAME"
-echo "$domain_name" > CNAME
-
-
-echo "Adding and committing changes"
-cp -R ../output/* .
-git add -A
-git commit -v
-git push origin gh-pages
-
-# Remove repo directory
-cd ..
-rm -rf "$repo_dir"
+aws s3 cp --recursive output/ "s3://$bucket/"
 
 # Done
 echo "** Remember to commit your source files"
