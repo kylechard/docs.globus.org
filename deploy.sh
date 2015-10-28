4d837bbcb957931b1be3d0ac70eb70299f4928a1
Stephen Rosen <sirosen@uchicago.edu>
2015-10-28 17:08:15 -0500

    Update deploy.sh to do s3 sync instead of cp
    Using sync is cheaper network-wise and therefore faster. Importantly, doing a
    delete sync after the upload removes any files that are unwanted from the
    bucket.
    It's safer to do two syncs, the second with --delete, than a single sync. It
    guarantees that the deletions won't happen until after the upload is complete.

--- deploy.sh
+++ deploy.sh
@@ -42,7 +42,13 @@ echo "Compiling site with nanoc"
 rm -rf output
 nanoc
 
-aws s3 cp --recursive output/ "s3://$bucket/"
+# copy output/ to S3
+echo -e "\n\n--- Sync to S3 ---\n"
+# do a non-delete sync to upload everything to the bucket
+aws s3 sync output/ "s3://$bucket/"
+# now do a delete sync to remove files absent from the output/ dir
+# done as a separate second step to ensure good order of operations
+aws s3 sync --delete output/ "s3://$bucket/"
 
 # Done
-echo "** Remember to commit your source files"
+echo -e "\n\n--- Success ---"
