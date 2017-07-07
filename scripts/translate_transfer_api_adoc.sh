118518d55436170f1b914602eccf26afc7cc4560
teresas <teresas@ci.uchicago.edu>
2017-07-07 15:21:23 -0500

    updated transfer api docs

--- scripts/translate_transfer_api_adoc.sh
+++ scripts/translate_transfer_api_adoc.sh
@@ -120,6 +120,9 @@ sed -i.bak -e 's/link:\([_a-z0-9]*\){outfilesuffix}/link:..\/\1/g' $DOC_DIR/*.ad
 sed -i.bak -e "s/^\:toc\: macro/${TOC}/g" $DOC_DIR/*.adoc
 sed -i.bak -e "/^toc\:\:\[\]/d" $DOC_DIR/*.adoc
 
+# This is for asciidoctor, not needed for asciidoc
+sed -i.bak -e "/^\:compat-mode\:/d" $DOC_DIR/*.adoc
+
 # Change history is an infinite list of headings, so don't number it
 # TODO: Do we need TOC at all on change_history?
 sed -i.bak -e 's/^\:numbered\://g' $DOC_DIR/change_history.adoc
